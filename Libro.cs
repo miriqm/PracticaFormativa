2c23ac579e74f11666c5ca7f472e9659e7011123
Miriam <120044756+miriqm@users.noreply.github.com>
2024-05-05 20:26:12 -0300

    Agregar archivos de proyecto.

---
+++ Libro.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaFormativa
+{
+    internal class Libro
+    {
+        private string titulo;
+        private string autor;
+        private string editorial;
+
+        // CONSTRUCTOR
+        public Libro(string titulo, string autor, string editorial)
+        {
+            this.Titulo = titulo;
+            this.Autor = autor;
+            this.Editorial = editorial;
+        }
+
+        public string Titulo { get => titulo; set => titulo = value; }
+        public string Autor { get => autor; set => autor = value; }
+        public string Editorial { get => editorial; set => editorial = value; }
+    }
+}
---
+++ semana3/frmInicio.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace PracticaFormativa.semana3
+{
+    partial class frmInicio
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(252, 90);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(222, 80);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Uso de una clase";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // frmInicio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Name = "frmInicio";
+            this.Text = "Uso de Clases";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+    }
+}
